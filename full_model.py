07636bd56b64f351aae51087bc6d7ae576872b31
Juan Camilo Sabogal Olarte <sabogalo@msu.edu>
2023-12-02 21:59:40 -0500

    Revised final model

--- full_model.py
+++ full_model.py
@@ -15,31 +15,34 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
+from joblib import Parallel, delayed
+
 import processing as ps
 
 from itertools import product
 
 START_DATE = "2021-01-01"
-END_DATE = "2024-01-01"
+END_DATE = "2023-12-01"
 TRAIN_SIZE = 0.9
 NUM_FOLDS = 5
 
 RANDOM_ITERATIONS = 50
 
-WINDOW_SIZE_VALUES = [3]  # [3, 5, 10, 20]
-BATCH_SIZE_VALUES = [16]  # [16, 32, 64, 128]
-HIDDEN_SIZE_VALUES = [64]  # [32, 64, 128]
-LEARNING_RATE_VALUES = [0.0001]  # [0.1, 0.01, 0.001, 0.0001]
-EPOCH_VALUES = [300]  # [50, 100, 300, 1000]
+WINDOW_SIZE_VALUES = [3, 5, 10]
+BATCH_SIZE_VALUES = [32, 64]
+HIDDEN_SIZE_VALUES = [32, 64, 128]
+LEARNING_RATE_VALUES = [0.1, 0.01, 0.001, 0.0001]
+EPOCH_VALUES = [100, 400, 700]
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim, device):
         super().__init__()
 
         self.hidden_dim = hidden_dim
         self.input_dim = input_dim
         self.num_layers = num_layers
+        self.device = device
 
         self.lstm = nn.LSTM(input_size=self.input_dim,
                             hidden_size=self.hidden_dim,
@@ -50,8 +53,8 @@ class LSTM(nn.Module):
     def forward(self, x):
         batch_size = x.shape[0]
 
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_()
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_()
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_().to(self.device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_().to(self.device)
 
         _, (hn, _) = self.lstm(x, (h0, c0))
         out = self.linear(hn[-1])  # Last hidden state
@@ -62,13 +65,14 @@ class LSTM(nn.Module):
 class PortfolioOptimizer:
     DEFAULT_DAILY_INVESTMENT_AMOUNT = 100
 
-    def __init__(self, ticker_symbols, start_date=START_DATE, end_date=END_DATE, train_size=TRAIN_SIZE):
+    def __init__(self, ticker_symbols, device, start_date=START_DATE, end_date=END_DATE, train_size=TRAIN_SIZE):
         self.ticker_symbols = ticker_symbols
         self.risk_tolerance = None
         self.method = None
         self.start_date = start_date
         self.end_date = end_date
         self.train_size = train_size
+        self.device = device
 
         self.num_testing_days = 0
 
@@ -97,7 +101,16 @@ class PortfolioOptimizer:
             data = yf.download(ticker_symbol, start=self.start_date, end=self.end_date)
             self.dataframes[ticker_symbol] = data
 
+    def print_data_summary(self):
+        example_df = self.dataframes[self.ticker_symbols[0]]
+        total_num_days = example_df.shape[0]
+        num_features = example_df.shape[1] - 1
+        print(f"Number of samples: {total_num_days}")
+        print(f"Number of features: {num_features}")
+
     def construct_features(self, target_feature="Returns"):
+        print("\nBefore preprocessing:")
+        self.print_data_summary()
         for ticker, ticker_df in self.dataframes.items():
 
             ticker_df.rename(columns={'Volume': f"Volume {ticker}",
@@ -118,8 +131,12 @@ class PortfolioOptimizer:
             [ticker_df[f"Returns {ticker}"].iloc[:int(self.train_size * len(ticker_df))]
              for ticker, ticker_df in self.dataframes.items()])
 
-        total_num_days = len(self.dataframes[self.ticker_symbols[0]])
+        print("\nAfter preprocessing:")
+        self.print_data_summary()
+        example_df = self.dataframes[self.ticker_symbols[0]]
+        total_num_days = example_df.shape[0]
         self.num_testing_days = total_num_days - int(self.train_size * total_num_days)
+        print(f"Number of testing days: {self.num_testing_days}")
 
     def set_method(self, method):
         self.method = method
@@ -130,27 +147,29 @@ class PortfolioOptimizer:
     def set_greedy_classification(self, greedy_classification):
         self.greedy_classification = greedy_classification
 
-    def cross_validate(self, full_data, num_folds=NUM_FOLDS, ticker_symbol=None):
-        best_error = float("inf")
-        best_params = None
+    def parallel_cross_validate(self, full_data, num_folds=NUM_FOLDS, ticker_symbol=None):
 
-        for (window_size, batch_size, hidden_dim, learning_rate, num_epochs) in product(WINDOW_SIZE_VALUES,
-                                                                                        BATCH_SIZE_VALUES,
-                                                                                        HIDDEN_SIZE_VALUES,
-                                                                                        LEARNING_RATE_VALUES,
-                                                                                        EPOCH_VALUES):
+        def evaluate_params(params):
+            window_size, batch_size, hidden_dim, learning_rate, num_epochs = params
             total_error = 0
             btscv = ps.BlockingTimeSeriesSplit(n_splits=num_folds, train_size=TRAIN_SIZE)
+
             for train_indices, test_indices in btscv.split(full_data, window_size):
                 train, test = full_data[train_indices], full_data[test_indices]
-                total_error += \
-                    self.train_and_test(train, test, window_size, batch_size, hidden_dim, learning_rate, num_epochs,
-                                        ticker_symbol)[0]
-            average_error = total_error / num_folds
-            if average_error < best_error:
-                best_error = average_error
-                best_params = (window_size, batch_size, hidden_dim, learning_rate, num_epochs)
+                total_error += self.train_and_test(train, test, window_size, batch_size,
+                                                   hidden_dim, learning_rate, num_epochs, ticker_symbol)[0]
 
+            average_error = total_error / num_folds
+            return average_error, (window_size, batch_size, hidden_dim, learning_rate, num_epochs)
+
+        params_list = list(product(WINDOW_SIZE_VALUES, BATCH_SIZE_VALUES, HIDDEN_SIZE_VALUES,
+                                   LEARNING_RATE_VALUES, EPOCH_VALUES))
+
+        results = Parallel(n_jobs=-1, verbose=6)(delayed(evaluate_params)(params) for params in params_list)
+
+        best_error, best_params = min(results, key=lambda x: x[0])
+        print("\nBest parameters: ws = {}, batch size = {}, hid = {}, lr = {}, epochs = {}".format(*best_params))
+        print("Lowest average error:", best_error, "\n")
         return best_params
 
     def train_and_test(self, train, test, window_size, batch_size, hidden_dim, learning_rate, num_epochs,
@@ -166,8 +185,10 @@ class PortfolioOptimizer:
 
             X_train, y_train = ps.create_regression_dataset(train_scaled, window_size=window_size)
             X_test, y_test = ps.create_regression_dataset(test_scaled, window_size=window_size)
+            X_train, X_test = X_train.to(self.device), X_test.to(self.device)
+            y_train, y_test = y_train.to(self.device), y_test.to(self.device)
 
-        elif self.method == "classification":
+        else:
             output_dim = len(self.ticker_symbols)
             loss_string = "Cross Entropy Loss"
             loss_fn = nn.CrossEntropyLoss()
@@ -175,10 +196,9 @@ class PortfolioOptimizer:
             train_features, train_labels = train[:, :-1], train[:, -1].reshape(-1, 1)
             test_features, test_labels = test[:, :-1], test[:, -1].reshape(-1, 1)
 
-            one_hot_train_labels = np.eye(3)[
-                train_labels.astype(int).reshape(-1)]  # Add one-hot encoding of labels from previous days as features
-            one_hot_test_labels = np.eye(3)[
-                test_labels.astype(int).reshape(-1)]  # Add one-hot encoding of labels from previous days as features
+            # Add one-hot encoding of labels from previous days as features
+            one_hot_train_labels = np.eye(len(self.ticker_symbols))[train_labels.astype(int).reshape(-1)]
+            one_hot_test_labels = np.eye(len(self.ticker_symbols))[test_labels.astype(int).reshape(-1)]
 
             scaler = RobustScaler()
             train_features_scaled = scaler.fit_transform(train_features)
@@ -189,8 +209,11 @@ class PortfolioOptimizer:
 
             X_train, y_train = ps.create_classification_dataset(train_dataset, window_size=window_size)
             X_test, y_test = ps.create_classification_dataset(test_dataset, window_size=window_size)
+            X_train, X_test, y_train, y_test = X_train.to(self.device), X_test.to(self.device), y_train.to(
+                self.device), y_test.to(self.device)
 
-        model = LSTM(input_dim=X_train.shape[-1], hidden_dim=hidden_dim, num_layers=1, output_dim=output_dim)
+        model = LSTM(input_dim=X_train.shape[-1], hidden_dim=hidden_dim, num_layers=1, output_dim=output_dim,
+                     device=self.device).to(self.device)
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
         loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
@@ -210,23 +233,26 @@ class PortfolioOptimizer:
             if epoch % 20 != 0:
                 continue
 
-            model.eval()
-            with torch.no_grad():
-                y_pred = model(X_train)
-                if self.method == "regression":
-                    # Repeat column to match dimension of scaler and get only last column (target) after descaling
-                    y_pred_orig = scaler.inverse_transform(np.repeat(y_pred, X_train.shape[2], axis=-1))[:, -1]
-                    y_train_orig = scaler.inverse_transform(np.repeat(y_train, X_train.shape[2], axis=-1))[:, -1]
-                    train_error = np.sqrt(mean_squared_error(y_train_orig, y_pred_orig))
+            if print_progress or display_metrics:
+                model.eval()
+                with torch.no_grad():
+                    y_pred = model(X_train)
+                    if self.method == "regression":
+                        # Repeat column to match dimension of scaler and get only last column (target) after descaling
+                        y_pred_orig = scaler.inverse_transform(
+                            np.repeat(y_pred.cpu().numpy(), X_train.shape[2], axis=-1))[:, -1]
+                        y_train_orig = scaler.inverse_transform(
+                            np.repeat(y_train.cpu().numpy(), X_train.shape[2], axis=-1))[:, -1]
+                        train_error = np.sqrt(mean_squared_error(y_train_orig, y_pred_orig))
 
-                elif self.method == "classification":
-                    train_error = loss_fn(y_pred, y_train).item()
+                    elif self.method == "classification":
+                        train_error = loss_fn(y_pred, y_train).item()
 
-                if print_progress:
-                    print(f"Epoch {epoch}: train {loss_string} = {train_error:.4f}")
-                if display_metrics:
-                    losses.append(train_error)
-                    epoch_list.append(epoch)
+                    if print_progress:
+                        print(f"Epoch {epoch}: train {loss_string} = {train_error:.4f}")
+                    if display_metrics:
+                        losses.append(train_error)
+                        epoch_list.append(epoch)
 
         if display_metrics:
             plt.figure()
@@ -235,7 +261,7 @@ class PortfolioOptimizer:
             plt.ylabel(loss_string)
             if self.method == "regression":
                 title = f"{ticker_symbol} Training {loss_string} vs Epoch"
-            elif self.method == "classification":
+            else:
                 title = f"Training {loss_string} vs Epoch "
             plt.title(title)
 
@@ -245,14 +271,14 @@ class PortfolioOptimizer:
             y_pred = model(X_test)
             if self.method == "regression":
                 # Repeat column to match dimension of scaler and get only last column (target) after descaling
-                y_pred_orig = scaler.inverse_transform(np.repeat(y_pred, X_test.shape[2], axis=-1))[:, -1]
-                y_test_orig = scaler.inverse_transform(np.repeat(y_test, X_test.shape[2], axis=-1))[:, -1]
+                y_pred_orig = scaler.inverse_transform(np.repeat(y_pred.cpu().numpy(), X_test.shape[2], axis=-1))[:, -1]
+                y_test_orig = scaler.inverse_transform(np.repeat(y_test.cpu().numpy(), X_test.shape[2], axis=-1))[:, -1]
                 test_error = np.sqrt(mean_squared_error(y_test_orig, y_pred_orig))
 
                 if display_metrics:
                     plt.figure(figsize=(16, 4))
-                    plt.plot(y_test_orig, color='black', label="actual")
-                    plt.plot(y_pred_orig, color='red', label="predicted")
+                    plt.plot(y_test_orig, 'o-k', label="actual")
+                    plt.plot(y_pred_orig, 'o-r', label="predicted")
                     plt.xlabel("Time (days)")
                     plt.ylabel("Returns")
                     plt.legend()
@@ -265,13 +291,14 @@ class PortfolioOptimizer:
                 test_error = loss_fn(y_pred, y_test).item()
                 if display_metrics:
                     y_pred_labels = torch.argmax(y_pred, dim=1)
-                    accuracy = accuracy_score(y_test, y_pred_labels)
+                    accuracy = accuracy_score(y_test.cpu().numpy(), y_pred_labels.cpu().numpy())
+                    print("Test Cross Entropy Loss:", test_error)
                     print("Testing accuracy:", accuracy)
-                    print(confusion_matrix(y_test, y_pred_labels))
-                    print(classification_report(y_test, y_pred_labels))
+                    print(confusion_matrix(y_test.cpu().numpy(), y_pred_labels.cpu().numpy()))
+                    print(classification_report(y_test.cpu().numpy(), y_pred_labels.cpu().numpy()))
 
                 if final_predictions:
-                    pred_probabilities = torch.softmax(y_pred, dim=1).numpy()
+                    pred_probabilities = torch.softmax(y_pred, dim=1).cpu().numpy()
                     self.classification_predictions = pred_probabilities
 
             return test_error, model
@@ -284,14 +311,14 @@ class PortfolioOptimizer:
             X_validation = X[:train_size]
 
             print(f"\nCross validating for {ticker_symbol}...")
-            window_size, batch_size, hidden_dim, learning_rate, num_epochs = self.cross_validate(X_validation,
-                                                                                                 ticker_symbol=ticker_symbol)
-            print("\nBest parameters found:", window_size, batch_size, hidden_dim, learning_rate, num_epochs, "\n")
+            best_params = self.parallel_cross_validate(X_validation, ticker_symbol=ticker_symbol)
+            window_size, batch_size, hidden_dim, learning_rate, num_epochs = best_params
             train, test = X[:train_size], X[train_size - window_size:]
-            self.best_regression_models[ticker_symbol] = \
-                self.train_and_test(train, test, window_size, batch_size, hidden_dim, learning_rate, num_epochs,
-                                    ticker_symbol, print_progress=False, display_metrics=True, final_predictions=True)[
-                    1]
+            self.best_regression_models[ticker_symbol] = self.train_and_test(train, test, window_size,
+                                                                             batch_size, hidden_dim, learning_rate,
+                                                                             num_epochs, ticker_symbol,
+                                                                             print_progress=False, display_metrics=True,
+                                                                             final_predictions=True)[1]
 
     def train_classification(self):
         full_df = pd.concat(self.dataframes.values(), axis=1)
@@ -301,17 +328,15 @@ class PortfolioOptimizer:
         X_validation = X[:train_size]
 
         print("\nCross validating for classification...")
-        window_size, batch_size, hidden_dim, learning_rate, num_epochs = self.cross_validate(X_validation)
-        print("\nBest parameters found:", window_size, batch_size, hidden_dim, learning_rate, num_epochs, "\n")
+        window_size, batch_size, hidden_dim, learning_rate, num_epochs = self.parallel_cross_validate(X_validation)
         train, test = X[:train_size], X[train_size - window_size:]
-        self.best_classification_model = \
-            self.train_and_test(train, test, window_size, batch_size, hidden_dim, learning_rate, num_epochs,
-                                print_progress=False, display_metrics=True, final_predictions=True)[1]
+        self.best_classification_model = self.train_and_test(train, test, window_size, batch_size, hidden_dim,
+                                                             learning_rate, num_epochs, print_progress=False,
+                                                             display_metrics=True, final_predictions=True)[1]
 
     def train(self):
         torch.manual_seed(0)
         np.random.seed(0)
-        torch.use_deterministic_algorithms(True)
 
         if self.method == "equal" or self.method == "random":
             return
@@ -338,7 +363,7 @@ class PortfolioOptimizer:
         elif self.method == "classification":
             weights = self.classification_predictions[day]
             if self.greedy_classification:
-                weights = np.eye(3)[np.argmax(weights)]
+                weights = np.eye(len(self.ticker_symbols))[np.argmax(weights)]
             return weights
 
         elif self.method == "random":
@@ -412,9 +437,10 @@ class PortfolioOptimizer:
         cumulative_portfolio_returns = np.cumprod(1 + np.array(portfolio_returns_list)) - 1
         ax2.plot(100 * cumulative_portfolio_returns)
         ax2.set_ylabel('Cumulative Portfolio Returns (in %)')
-        secax = ax2.secondary_yaxis('right', functions=(lambda x: self.DEFAULT_DAILY_INVESTMENT_AMOUNT * (x / 100 + 1),
+        secondary_axis = ax2.secondary_yaxis('right',
+                                             functions=(lambda x: self.DEFAULT_DAILY_INVESTMENT_AMOUNT * (x / 100 + 1),
                                                         lambda x: 100 * (x / self.DEFAULT_DAILY_INVESTMENT_AMOUNT - 1)))
-        secax.set_ylabel('Capital (in USD)')
+        secondary_axis.set_ylabel('Capital (in USD)')
         plt.xlabel('Day')
         plt.title("Cumulative Portfolio Performance")
 
--- processing.py
+++ processing.py
@@ -5,23 +5,28 @@ import numpy as np
 from cvxopt import matrix 
 from cvxopt import solvers
 
-SMA_PERIOD = 20
-EMA_PERIOD = 20
-CCI_PERIOD = 20
-VOLATILITY_PERIOD = 20
-ROC_PERIOD = 20
+SMA_PERIOD = 10
+EMA_PERIOD = 10
+CCI_PERIOD = 10
+VOLATILITY_PERIOD = 10
+ROC_PERIOD = 10
+
 
 def returns(df, ticker):
     return (df[f"Close {ticker}"] - df[f"Open {ticker}"]) / df[f"Open {ticker}"]
 
+
 def log_returns(df, ticker):
     return np.log(df[f"Close {ticker}"]).diff()
+
 
 def sma(df, ticker):
     return df[f"Close {ticker}"].rolling(window=SMA_PERIOD).mean()
 
+
 def ema(df, ticker):
     return df[f"Close {ticker}"].ewm(span=EMA_PERIOD, adjust=False).mean()
+
 
 def vwap(df, ticker):
     value = df[f"Close {ticker}"] * df[f"Volume {ticker}"]
@@ -29,17 +34,21 @@ def vwap(df, ticker):
     cumulative_volume = df[f"Volume {ticker}"].cumsum()
     return cumulative_value / cumulative_volume
 
+
 def cci(df, ticker):
     typical_price = (df[f"High {ticker}"] + df[f"Low {ticker}"] + df[f"Close {ticker}"]) / 3
     mean_typical_price = typical_price.rolling(window=CCI_PERIOD).mean()
     mean_deviation = (typical_price - mean_typical_price).abs().rolling(window=CCI_PERIOD).mean()
     return (typical_price - mean_typical_price) / (0.015 * mean_deviation)
 
+
 def volatility(df, ticker):
     return df[f"Returns {ticker}"].rolling(window=VOLATILITY_PERIOD).std()
 
+
 def roc(df, ticker):
     return (df[f"Close {ticker}"] / df[f"Close {ticker}"].shift(ROC_PERIOD) - 1) * 100
+
 
 def create_regression_dataset(dataset, window_size):
     X, y = [], []
@@ -47,7 +56,8 @@ def create_regression_dataset(dataset, window_size):
         features, target = dataset[i:i+window_size], dataset[i+window_size, -1]
         X.append(features)
         y.append(target)
-    return torch.tensor(np.array(X), dtype=torch.float), torch.tensor(np.array(y), dtype=torch.float).reshape(-1,1)
+    return torch.tensor(np.array(X), dtype=torch.float), torch.tensor(np.array(y), dtype=torch.float).reshape(-1, 1)
+
 
 def create_classification_dataset(dataset, window_size):
     X, y = [], []
@@ -57,14 +67,15 @@ def create_classification_dataset(dataset, window_size):
         y.append(target)
     return torch.tensor(np.array(X), dtype=torch.float), torch.tensor(np.array(y), dtype=torch.long)
 
-def markowitz_mean_variance(returns, covariance_matrix, risk_tolerance=0):
+
+def markowitz_mean_variance(returns_list, covariance_matrix, risk_tolerance=0):
     """ Compute the optimal weights using the Markowitz mean-variance model.
 
     The goal is to maximize the expected returns of a portfolio for a given level of risk.
     Since we don't allow short selling (negative weights), we use G and h in cvxopt to indicate the inequality w_i >= 0.
     
     Args:
-        returns: Returns of the tickers
+        returns_list: Array/list of the tickers' returns
         covariance_matrix: Covariance matrix of the returns of the tickers
         risk_tolerance: Risk tolerance parameter
 
@@ -72,11 +83,12 @@ def markowitz_mean_variance(returns, covariance_matrix, risk_tolerance=0):
         The optimal weights
     """
     solvers.options["show_progress"] = False
+    returns_float = returns_list.astype(float)
     P = covariance_matrix
-    q = -risk_tolerance * returns
-    G = -np.eye(len(returns))
-    h = np.zeros((len(returns),1))
-    A = np.ones((1, len(returns)))
+    q = -risk_tolerance * returns_float
+    G = -np.eye(len(returns_float))
+    h = np.zeros((len(returns_float), 1))
+    A = np.ones((1, len(returns_float)))
     b = 1.0
 
     P = matrix(P)
@@ -90,8 +102,9 @@ def markowitz_mean_variance(returns, covariance_matrix, risk_tolerance=0):
     
     return np.array(sol["x"]).squeeze()
 
+
 # Adapted from https://goldinlocks.github.io/Time-Series-Cross-Validation/
-class BlockingTimeSeriesSplit():
+class BlockingTimeSeriesSplit:
     def __init__(self, n_splits, train_size):
         self.n_splits = n_splits
         self.train_size = train_size
